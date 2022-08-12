14cdacbc2a65cf9b5949f7262a5df2c329edf983
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-08-12 14:30:38 -0300

    Primeiro Projeto Java em Intelij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("", "", 0);
+        System.out.println(gato);
+        int a = 3;
+        int b = 5;
+        System.out.println("Hello World!" + (a + b));
+    }
+}
